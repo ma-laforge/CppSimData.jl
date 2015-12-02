866cd8c3657701301bb891f1877ae1e4de4e5e65
MA Laforge <ma.laforge@example.com>
2015-12-02 17:27:33 -0500

    Use joinpath() more rigorously & remove @show.

--- src/CppSimData.jl
+++ src/CppSimData.jl
@@ -7,13 +7,11 @@ const libroot = joinpath(rootpath, "core/lib")
 
 #TODO: is Sys.ARCH a supported feature??
 const objfile = joinpath(libroot,
-@windows? ("win32/cppsimdata_lib.dll"
-):@osx? ("macosx/cppsimdata_lib.so"
+@windows? (joinpath("win32", "cppsimdata_lib.dll")
+):@osx? (joinpath("macosx", "cppsimdata_lib.so")
 ): joinpath(:x86_64 == Sys.ARCH?"glnxa64":"glnx86", "cppsimdata_lib.so")
 )
 #else: error("OS not supported: ", Sys.OS_NAME))??
-
-@show objfile
 
 include("base.jl")
 include("show.jl")
